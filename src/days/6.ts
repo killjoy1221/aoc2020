faf2ce4f444203be27ff0638bdc53f97713040d4
Matthew Messinger <mattmess1221@gmail.com>
2020-12-06 13:13:40 -0500

    Day 6

---
+++ src/days/6.ts
@@ -0,0 +1,26 @@
+import { Puzzle } from '../index';
+import { intersect, sum, union } from '../util';
+
+export class Day6 implements Puzzle {
+    private readonly data: Set<string>[][];
+
+    constructor (buffer: Buffer) {
+        this.data = buffer.toString('utf-8')
+            .split(/(\r?\n){2}/)
+            .map(s => s.trim())
+            .filter(s => s)
+            .map(s => s.split(/\r?\n/).map(s => new Set(s)));
+    }
+
+    solvePart1 (): void {
+        // union of sets
+        const unions = this.data.map(group => union(...group));
+        console.log(sum(unions.map(group => group.size)));
+    }
+
+    solvePart2 (): void {
+        // intersect of sets
+        const intersects = this.data.map(group => intersect(...group));
+        console.log(sum(intersects.map(group => group.size)));
+    }
+}
