5f022cb69e1694e9cdeb2ea4bee22afe8eb5b4a1
Matthew Messinger <mattmess1221@gmail.com>
2020-12-12 20:01:44 -0500

    Day 6

--- src/util.ts
+++ src/util.ts
@@ -13,3 +13,26 @@ export function sum (numbers: number[]) {
 export function prod (numbers: number[]) {
     return numbers.reduce(mul, 1);
 }
+
+/**
+ * Calculates a union set from an array of Sets
+ * @param sets The sets to use
+ */
+export function union<T> (...sets: Set<T>[]): Set<T> {
+    return new Set(sets.flatMap(s => [...s]));
+}
+
+/**
+ * Calculates an intersect set from an array of Sets.
+ * @param sets The sets to use
+ */
+export function intersect<T> (...sets: Set<T>[]): Set<T> {
+    if (sets.length === 0) {
+        return new Set();
+    }
+    const first = sets[0];
+    sets = sets.slice(1);
+    return sets.reduce((a, b) => {
+        return new Set([...a].filter(x => b.has(x)));
+    }, first);
+}
